b9bb061ad976ee9bcc3aa7cb8b07ad112da250fb
HamidrezaBahmani <hb.hamidrezabahmani.com@gmail.com>
2023-11-19 01:23:04 +0330

    Adding a Meals Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 }
