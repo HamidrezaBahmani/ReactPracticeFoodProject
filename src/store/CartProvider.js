c366f6e25cde4579e9c23a5085e4183f20976642
HamidrezaBahmani <hb.hamidrezabahmani.com@gmail.com>
2023-11-21 00:36:10 +0330

    Outputting Cart Items

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -21,7 +21,7 @@ const cartReducer = (state, action) => {
   if (action.type === "ADD") {
     const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount =
-      state.totalAmount + action.item.price * action.item.amoun;
+      state.totalAmount + action.item.price * action.item.amount;
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
