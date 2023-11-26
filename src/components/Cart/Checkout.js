559616d7c64dd413b83d95cfc1162c31c5111fbc
HamidrezaBahmani <hb.hamidrezabahmani.com@gmail.com>
2023-11-26 19:21:56 +0330

    Submitting Sending Cart Data

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -44,6 +44,13 @@ const Checkout = (props) => {
     if (!formIsValid) {
       return;
     }
+
+    props.onConfirm({
+      name: enteredName,
+      street: enteredCity,
+      city: enteredCity,
+      postalCode: enteredPostalCode,
+    });
   };
 
   const nameControlClasses = `${classes.control} ${
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,8 +1,10 @@
-import classes from "./Modal.module.css";
-import ReactDOM from "react-dom";
+import { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+
+import classes from './Modal.module.css';
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop} onClick={props.onClose}></div>;
+  return <div className={classes.backdrop} onClick={props.onClose}/>;
 };
 
 const ModalOverlay = (props) => {
@@ -13,19 +15,17 @@ const ModalOverlay = (props) => {
   );
 };
 
-const portalElement = document.getElementById("overlays");
+const portalElement = document.getElementById('overlays');
+
 const Modal = (props) => {
   return (
-    <>
-      {ReactDOM.createPortal(
-        <Backdrop onClose={props.onClose} />,
-        portalElement
-      )}
+    <Fragment>
+      {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
       )}
-    </>
+    </Fragment>
   );
 };
 
